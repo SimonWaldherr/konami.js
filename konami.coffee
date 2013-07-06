36ad819675d05f018d8853de34a47dce2c4668aa
Simon Waldherr <git@simon.waldherr.eu>
2013-07-06 18:02:38 +0200

    code improvements, functions in objects, jslint/jshint valid (v0.3.0)

--- konami.coffee
+++ konami.coffee
@@ -1,83 +1,125 @@
 # * * * * * * * * * *
 # *    konami.js    *
-# *  Version 0.2.3  *
+# *  Version 0.3.0  *
 # *  License:  MIT  *
 # * Simon  Waldherr *
 # * * * * * * * * * *
 
-#jslint browser: true, plusplus: true, indent: 2 
+konami =
+  init: (konamicallback) ->
+    "use strict"
+    konami.db.lastkey = false
+    konami.db.string = (if (localStorage.getItem("konamistring") isnt null) then localStorage.getItem("konamistring") else "")
+    konami.db.kcodes = ""
+    konami.db[16] = {}
+    konami.db[16].status = false
+    konami.db[20] = {}
+    konami.db[20].status = false
+    addEventListener "keydown", (event) ->
+      konami.db[parseInt(event.keyCode, 10)] = {}  if konami.db[event.keyCode] is `undefined`
+      konami.db[parseInt(event.keyCode, 10)].status = true
+      konami.db[parseInt(event.keyCode, 10)].last = event.timeStamp
+      if (konami.db[16].status or konami.db[20].status) and not (konami.db[16].status and konami.db[20].status)
+        konami.db.uc = true
+      else
+        konami.db.uc = false
 
-#global addEventListener, console 
-konamidb = {}
-konami_init = ->
-  konamidb.lastkey = false
-  konamidb.string = (if (localStorage.getItem("konamistring") isnt null) then localStorage.getItem("konamistring") else "")
-  konamidb.kcodes = ""
-  konamidb[16] = {}
-  konamidb[16].status = false
-  konamidb[20] = {}
-  konamidb[20].status = false
-  addEventListener "keydown", (event) ->
-    konamidb[parseInt(event.keyCode, 10)] = {}  if konamidb[event.keyCode] is `undefined`
-    konamidb[parseInt(event.keyCode, 10)].status = true
-    konamidb[parseInt(event.keyCode, 10)].last = event.timeStamp
-    if (konamidb[16].status or konamidb[20].status) and not (konamidb[16].status and konamidb[20].status)
-      konamidb.uc = true
-    else
-      konamidb.uc = false
+    addEventListener "keyup", (event) ->
+      key = (if (konami.db.uc) then String.fromCharCode(event.keyCode).toUpperCase() else String.fromCharCode(event.keyCode).toLowerCase())
+      key = (if ((event.keyCode > 31) and (event.keyCode < 127) and (event.keyCode isnt 91)) then key else "")
+      konami.db[parseInt(event.keyCode, 10)] = {}  if konami.db[event.keyCode] is `undefined`
+      konami.db[parseInt(event.keyCode, 10)].status = false
+      konami.db[parseInt(event.keyCode, 10)].last = event.timeStamp
+      konami.db.lastkey = event.keyCode
+      if event.keyCode isnt 8
+        konami.db.string = konami.db.string.substr(-42, 42) + key  unless event.keyCode > 36 and event.keyCode < 41
+        konami.db.kcodes = konami.db.kcodes.substr(-42, 42) + event.keyCode
+      else
+        konami.db.string = " " + konami.db.string.substr(-42, 41) + key
+        konami.db.kcodes = "  " + konami.db.kcodes.substr(-42, 40) + event.keyCode
 
-  addEventListener "keyup", (event) ->
-    key = (if (konamidb.uc) then String.fromCharCode(event.keyCode).toUpperCase() else String.fromCharCode(event.keyCode).toLowerCase())
-    key = (if ((event.keyCode > 31) and (event.keyCode < 127) and (event.keyCode isnt 91)) then key else "")
-    konamidb[parseInt(event.keyCode, 10)] = {}  if konamidb[event.keyCode] is `undefined`
-    konamidb[parseInt(event.keyCode, 10)].status = false
-    konamidb[parseInt(event.keyCode, 10)].last = event.timeStamp
-    konamidb.lastkey = event.keyCode
-    if event.keyCode isnt 8
-      konamidb.string = konamidb.string.substr(-42, 42) + key
-      konamidb.kcodes = konamidb.kcodes.substr(-42, 42) + event.keyCode
-    else
-      konamidb.string = " " + konamidb.string.substr(-42, 41) + key
-      konamidb.kcodes = "  " + konamidb.kcodes.substr(-42, 40) + event.keyCode
-    console.log "konami"  if konamidb.kcodes.substr(-20, 20) is "38384040373937396665"
-    if (konamidb[16].status or konamidb[20].status) and not (konamidb[16].status and konamidb[20].status)
-      konamidb.uc = true
-    else
-      konamidb.uc = false
+      #up up down down left right left right B A
+      konamicallback()  if typeof konamicallback is "function"  if konami.db.kcodes.substr(-20, 20) is "38384040373937396665"
+      if (konami.db[16].status or konami.db[20].status) and not (konami.db[16].status and konami.db[20].status)
+        konami.db.uc = true
+      else
+        konami.db.uc = false
 
-  addEventListener "beforeunload", ->
-    localStorage.setItem "konamistring", konamidb.string
-
-
-konami_get = (key) ->
-  [konamidb[key.toUpperCase().charCodeAt(0)].status, konamidb[key.toUpperCase().charCodeAt(0)].last]
-
-konami_reg = (key, callback) ->
-  addEventListener "keydown", (event) ->
-    pressedKey = parseInt(event.keyCode, 10)
-    if pressedKey is 16
-      pressedKey = "shift"
-    else if pressedKey is 18
-      pressedKey = "alt"
-    else if pressedKey is 91
-      pressedKey = "cmd"
-    else if pressedKey is 17
-      pressedKey = "ctrl"
-    else if pressedKey is 37
-      pressedKey = "left"
-    else if pressedKey is 39
-      pressedKey = "right"
-    else if pressedKey is 38
-      pressedKey = "up"
-    else if pressedKey is 40
-      pressedKey = "down"
-    else if pressedKey is 13
-      pressedKey = "return"
-    else if pressedKey is 8
-      pressedKey = "backspace"
-    else
-      pressedKey = (if (konamidb.uc) then String.fromCharCode(pressedKey).toUpperCase() else String.fromCharCode(pressedKey).toLowerCase())
-    callback()  if pressedKey is key
+    addEventListener "beforeunload", ->
+      localStorage.setItem "konamistring", konami.db.string
 
 
-window.onload = konami_init()
+  get: (key) ->
+    "use strict"
+    keystatus = (if konami.db[key.toUpperCase().charCodeAt(0)] is `undefined` then null else konami.db[key.toUpperCase().charCodeAt(0)].status)
+    lastpressd = (if konami.db[key.toUpperCase().charCodeAt(0)] is `undefined` then null else konami.db[key.toUpperCase().charCodeAt(0)].last)
+    [keystatus, lastpressd]
+
+  reg: (key, callback) ->
+    "use strict"
+    addEventListener "keydown", (event) ->
+      pressedKey = parseInt(event.keyCode, 10)
+      keys_status = true
+      i = 0
+      keys = undefined
+      if typeof key is "string"
+        if pressedKey is 16
+          pressedKey = "shift"
+        else if pressedKey is 18
+          pressedKey = "alt"
+        else if pressedKey is 91
+          pressedKey = "cmd"
+        else if pressedKey is 17
+          pressedKey = "ctrl"
+        else if pressedKey is 37
+          pressedKey = "left"
+        else if pressedKey is 39
+          pressedKey = "right"
+        else if pressedKey is 38
+          pressedKey = "up"
+        else if pressedKey is 40
+          pressedKey = "down"
+        else if pressedKey is 13
+          pressedKey = "return"
+        else if pressedKey is 8
+          pressedKey = "backspace"
+        else
+          pressedKey = (if (konami.db.uc) then String.fromCharCode(pressedKey).toUpperCase() else String.fromCharCode(pressedKey).toLowerCase())
+      else
+        for keys of key
+          if typeof key[keys] is "string"
+            if key[keys] is "shift"
+              key[keys] = 16
+            else if key[keys] is "alt"
+              key[keys] = 18
+            else if key[keys] is "cmd"
+              key[keys] = 91
+            else if key[keys] is "ctrl"
+              key[keys] = 17
+            else if key[keys] is "left"
+              key[keys] = 37
+            else if key[keys] is "right"
+              key[keys] = 39
+            else if key[keys] is "up"
+              key[keys] = 38
+            else if key[keys] is "down"
+              key[keys] = 40
+            else if key[keys] is "return"
+              key[keys] = 13
+            else if key[keys] is "backspace"
+              key[keys] = 8
+            else key[keys] = key[keys].charCodeAt(0)  if key[keys].length is 1
+      if typeof key is "string"
+        callback()  if pressedKey is key
+      else
+        i = 0
+        while i < key.length
+          if konami.db[key[i]] isnt `undefined`
+            keys_status = false  unless konami.db[key[i]].status
+          else
+            keys_status = false
+          i++
+        callback()  if keys_status
+
+
+  db: {}
--- konami.js
+++ konami.js
@@ -1,102 +1,159 @@
 /* * * * * * * * * *
- *  konami.js  *
- *  Version 0.2.3  *
+ *    konami.js    *
+ *  Version 0.3.0  *
  *  License:  MIT  *
  * Simon  Waldherr *
  * * * * * * * * * */
 
 /*jslint browser: true, plusplus: true, indent: 2 */
-/*global addEventListener, console */
+/*global addEventListener */
 
-var konamidb = {};
+var konami = {
+  init : function (konamicallback) {
+    "use strict";
+    konami.db.lastkey = false;
+    konami.db.string = (localStorage.getItem('konamistring') !== null) ? localStorage.getItem('konamistring') : '';
+    konami.db.kcodes = '';
+    konami.db[16] = {};
+    konami.db[16].status = false;
+    konami.db[20] = {};
+    konami.db[20].status = false;
 
-var konami_init = function () {
-  konamidb.lastkey = false;
-  konamidb.string = (localStorage.getItem('konamistring') !== null) ? localStorage.getItem('konamistring') : '';
-  konamidb.kcodes = '';
-  konamidb[16] = {};
-  konamidb[16].status = false;
-  konamidb[20] = {};
-  konamidb[20].status = false;
+    addEventListener("keydown", function (event) {
+      if (konami.db[event.keyCode] === undefined) {
+        konami.db[parseInt(event.keyCode, 10)] = {};
+      }
+      konami.db[parseInt(event.keyCode, 10)].status = true;
+      konami.db[parseInt(event.keyCode, 10)].last = event.timeStamp;
+      if ((konami.db[16].status || konami.db[20].status) && !(konami.db[16].status && konami.db[20].status)) {
+        konami.db.uc = true;
+      } else {
+        konami.db.uc = false;
+      }
+    });
+    addEventListener("keyup", function (event) {
+      var key = (konami.db.uc) ? String.fromCharCode(event.keyCode).toUpperCase() : String.fromCharCode(event.keyCode).toLowerCase();
+      key = ((event.keyCode > 31) && (event.keyCode < 127) && (event.keyCode !== 91)) ? key : '';
+      if (konami.db[event.keyCode] === undefined) {
+        konami.db[parseInt(event.keyCode, 10)] = {};
+      }
 
-  addEventListener("keydown", function (event) {
-    if (konamidb[event.keyCode] === undefined) {
-      konamidb[parseInt(event.keyCode, 10)] = {};
-    }
-    konamidb[parseInt(event.keyCode, 10)].status = true;
-    konamidb[parseInt(event.keyCode, 10)].last = event.timeStamp;
-    if ((konamidb[16].status || konamidb[20].status) && !(konamidb[16].status && konamidb[20].status)) {
-      konamidb.uc = true;
-    } else {
-      konamidb.uc = false;
-    }
-  });
-  addEventListener("keyup", function (event) {
-    var key = (konamidb.uc) ? String.fromCharCode(event.keyCode).toUpperCase() : String.fromCharCode(event.keyCode).toLowerCase();
-    key = ((event.keyCode > 31) && (event.keyCode < 127) && (event.keyCode !== 91)) ? key : '';
-    if (konamidb[event.keyCode] === undefined) {
-      konamidb[parseInt(event.keyCode, 10)] = {};
-    }
+      konami.db[parseInt(event.keyCode, 10)].status = false;
+      konami.db[parseInt(event.keyCode, 10)].last = event.timeStamp;
 
-    konamidb[parseInt(event.keyCode, 10)].status = false;
-    konamidb[parseInt(event.keyCode, 10)].last = event.timeStamp;
+      konami.db.lastkey = event.keyCode;
+      if (event.keyCode !== 8) {
+        if (!(event.keyCode > 36 && event.keyCode < 41)) {
+          konami.db.string = konami.db.string.substr(-42, 42) + key;
+        }
+        konami.db.kcodes = konami.db.kcodes.substr(-42, 42) + event.keyCode;
+      } else {
+        konami.db.string = ' ' + konami.db.string.substr(-42, 41) + key;
+        konami.db.kcodes = '  ' + konami.db.kcodes.substr(-42, 40) + event.keyCode;
+      }
+      if (konami.db.kcodes.substr(-20, 20) === '38384040373937396665') {
+        //up up down down left right left right B A
+        if (typeof konamicallback === 'function') {
+          konamicallback();
+        }
+      }
+      if ((konami.db[16].status || konami.db[20].status) && !(konami.db[16].status && konami.db[20].status)) {
+        konami.db.uc = true;
+      } else {
+        konami.db.uc = false;
+      }
+    });
+    addEventListener("beforeunload", function () {
+      localStorage.setItem("konamistring", konami.db.string);
+    });
+  },
+  get : function (key) {
+    "use strict";
+    var keystatus = konami.db[key.toUpperCase().charCodeAt(0)] === undefined ? null : konami.db[key.toUpperCase().charCodeAt(0)].status,
+      lastpressd = konami.db[key.toUpperCase().charCodeAt(0)] === undefined ? null : konami.db[key.toUpperCase().charCodeAt(0)].last;
 
-    konamidb.lastkey = event.keyCode;
-    if (event.keyCode !== 8) {
-      konamidb.string = konamidb.string.substr(-42, 42) + key;
-      konamidb.kcodes = konamidb.kcodes.substr(-42, 42) + event.keyCode;
-    } else {
-      konamidb.string = ' ' + konamidb.string.substr(-42, 41) + key;
-      konamidb.kcodes = '  ' + konamidb.kcodes.substr(-42, 40) + event.keyCode;
-    }
-    if (konamidb.kcodes.substr(-20, 20) === '38384040373937396665') {
-      console.log('konami');
-    }
-    if ((konamidb[16].status || konamidb[20].status) && !(konamidb[16].status && konamidb[20].status)) {
-      konamidb.uc = true;
-    } else {
-      konamidb.uc = false;
-    }
-  });
-  addEventListener("beforeunload", function () {
-    localStorage.setItem("konamistring", konamidb.string);
-  });
+    return [keystatus, lastpressd];
+  },
+  reg : function (key, callback) {
+    "use strict";
+    addEventListener("keydown", function (event) {
+      var pressedKey = parseInt(event.keyCode, 10),
+        keys_status = true,
+        i = 0,
+        keys;
+
+      if (typeof key === 'string') {
+        if (pressedKey === 16) {
+          pressedKey = 'shift';
+        } else if (pressedKey === 18) {
+          pressedKey = 'alt';
+        } else if (pressedKey === 91) {
+          pressedKey = 'cmd';
+        } else if (pressedKey === 17) {
+          pressedKey = 'ctrl';
+        } else if (pressedKey === 37) {
+          pressedKey = 'left';
+        } else if (pressedKey === 39) {
+          pressedKey = 'right';
+        } else if (pressedKey === 38) {
+          pressedKey = 'up';
+        } else if (pressedKey === 40) {
+          pressedKey = 'down';
+        } else if (pressedKey === 13) {
+          pressedKey = 'return';
+        } else if (pressedKey === 8) {
+          pressedKey = 'backspace';
+        } else {
+          pressedKey = (konami.db.uc) ? String.fromCharCode(pressedKey).toUpperCase() : String.fromCharCode(pressedKey).toLowerCase();
+        }
+      } else {
+        for (keys in key) {
+          if (typeof key[keys] === 'string') {
+            if (key[keys] === 'shift') {
+              key[keys] = 16;
+            } else if (key[keys] === 'alt') {
+              key[keys] = 18;
+            } else if (key[keys] === 'cmd') {
+              key[keys] = 91;
+            } else if (key[keys] === 'ctrl') {
+              key[keys] = 17;
+            } else if (key[keys] === 'left') {
+              key[keys] = 37;
+            } else if (key[keys] === 'right') {
+              key[keys] = 39;
+            } else if (key[keys] === 'up') {
+              key[keys] = 38;
+            } else if (key[keys] === 'down') {
+              key[keys] = 40;
+            } else if (key[keys] === 'return') {
+              key[keys] = 13;
+            } else if (key[keys] === 'backspace') {
+              key[keys] = 8;
+            } else if (key[keys].length === 1) {
+              key[keys] = key[keys].charCodeAt(0);
+            }
+          }
+        }
+      }
+      if (typeof key === 'string') {
+        if (pressedKey === key) {
+          callback();
+        }
+      } else {
+        for (i = 0; i < key.length; i++) {
+          if (konami.db[key[i]] !== undefined) {
+            if (!konami.db[key[i]].status) {
+              keys_status = false;
+            }
+          } else {
+            keys_status = false;
+          }
+        }
+        if (keys_status) {
+          callback();
+        }
+      }
+    });
+  },
+  db : {}
 };
-
-var konami_get = function (key) {
-  return [konamidb[key.toUpperCase().charCodeAt(0)].status, konamidb[key.toUpperCase().charCodeAt(0)].last];
-};
-
-var konami_reg = function (key, callback) {
-  addEventListener("keydown", function (event) {
-    var pressedKey = parseInt(event.keyCode, 10);
-    if (pressedKey === 16) {
-      pressedKey = 'shift';
-    } else if (pressedKey === 18) {
-      pressedKey = 'alt';
-    } else if (pressedKey === 91) {
-      pressedKey = 'cmd';
-    } else if (pressedKey === 17) {
-      pressedKey = 'ctrl';
-    } else if (pressedKey === 37) {
-      pressedKey = 'left';
-    } else if (pressedKey === 39) {
-      pressedKey = 'right';
-    } else if (pressedKey === 38) {
-      pressedKey = 'up';
-    } else if (pressedKey === 40) {
-      pressedKey = 'down';
-    } else if (pressedKey === 13) {
-      pressedKey = 'return';
-    } else if (pressedKey === 8) {
-      pressedKey = 'backspace';
-    } else {
-      pressedKey = (konamidb.uc) ? String.fromCharCode(pressedKey).toUpperCase() : String.fromCharCode(pressedKey).toLowerCase();
-    }
-    if (pressedKey === key) {
-      callback();
-    }
-  });
-};
-
-window.onload = konami_init();
